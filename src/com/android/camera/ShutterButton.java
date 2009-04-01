0435bac841f165b3bda6e8ef38ce6b5ac6f0f2a5
Chih-Chung Chang <>
2009-04-01 00:11:56 -0700

    AI 143951: Style fix.
    Automated import of CL 143951

--- src/com/android/camera/ShutterButton.java
+++ src/com/android/camera/ShutterButton.java
@@ -22,11 +22,13 @@ import android.widget.ImageView;
 
 /**
  * A button designed to be used for the on-screen shutter button.
- * It's currently an ImageView that can call a delegate when the pressed state changes.
+ * It's currently an ImageView that can call a delegate when the pressed state
+ * changes.
  */
 public class ShutterButton extends ImageView {
     /**
-     * Interface definition for a callback to be invoked when a ModeButton's pressed state changes.
+     * Interface definition for a callback to be invoked when a ModeButton's
+     * pressed state changes.
      */
     public interface OnShutterButtonListener {
         /**
@@ -59,7 +61,8 @@ public class ShutterButton extends ImageView {
 
     /**
      * Hook into the drawable state changing to get changes to isPressed -- the
-     * onPressed listener doesn't always get called when the pressed state changes.
+     * onPressed listener doesn't always get called when the pressed state
+     * changes.
      */
     @Override
     protected void drawableStateChanged() {
@@ -67,24 +70,26 @@ public class ShutterButton extends ImageView {
         final boolean pressed = isPressed();
         if (pressed != mOldPressed) {
             if (!pressed) {
-                // When pressing the physical camera button the sequence of events is:
+                // When pressing the physical camera button the sequence of
+                // events is:
                 //    focus pressed, optional camera pressed, focus released.
-                // We want to emulate this sequence of events with the shutter button.
-                // When clicking using a trackball button, the view system changes
-                // the the drawable state before posting click notification, so the
-                // sequence of events is:
+                // We want to emulate this sequence of events with the shutter
+                // button. When clicking using a trackball button, the view
+                // system changes the the drawable state before posting click
+                // notification, so the sequence of events is:
                 //    pressed(true), optional click, pressed(false)
                 // When clicking using touch events, the view system changes the
-                // drawable state after posting click notification, so the sequence of
-                // events is:
+                // drawable state after posting click notification, so the
+                // sequence of events is:
                 //    pressed(true), pressed(false), optional click
-                // Since we're emulating the physical camera button, we want to have the
-                // same order of events. So we want the optional click callback to be delivered
-                // before the pressed(false) callback.
+                // Since we're emulating the physical camera button, we want to
+                // have the same order of events. So we want the optional click
+                // callback to be delivered before the pressed(false) callback.
                 //
-                // To do this, we delay the posting of the pressed(false) event slightly by
-                // pushing it on the event queue. This moves it after the optional click
-                // notification, so our client always sees events in this sequence:
+                // To do this, we delay the posting of the pressed(false) event
+                // slightly by pushing it on the event queue. This moves it
+                // after the optional click notification, so our client always
+                // sees events in this sequence:
                 //     pressed(true), optional click, pressed(false)
                 post(new Runnable() {
                     public void run() {
