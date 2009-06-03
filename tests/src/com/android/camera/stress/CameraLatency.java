11cac263ec966e5a5ac751c53acb851de841c62e
Yu Shan Emily Lau <yslau@google.com>
2009-06-02 21:59:58 -0700

    Exposed the camera latency measurement to the test. Added the camera latency to the StressTest runner.

--- tests/src/com/android/camera/stress/CameraLatency.java
+++ tests/src/com/android/camera/stress/CameraLatency.java
@@ -31,8 +31,19 @@ import android.view.KeyEvent;
 
 public class CameraLatency extends ActivityInstrumentationTestCase2 <Camera> {
     private String TAG = "CameraLatency";
-    private static final int TOTAL_NUMBER_OF_IMAGECAPTURE = 5;
+    private static final int TOTAL_NUMBER_OF_IMAGECAPTURE = 20;
     private static final long WAIT_FOR_IMAGE_CAPTURE_TO_BE_TAKEN = 3000;
+
+    private long mTotalAutoFocusTime;
+    private long mTotalShutterLag;
+    private long mTotalShutterAndRawPictureCallbackTime;
+    private long mTotalJpegPictureCallbackTimeLag;
+    private long mTotalRawPictureAndJpegPictureCallbackTime;
+    private long mAvgAutoFocusTime;
+    private long mAvgShutterLag = mTotalShutterLag;
+    private long mAvgShutterAndRawPictureCallbackTime;
+    private long mAveJpegPictureCallbackTimeLag;
+    private long mAvgRawPictureAndJpegPictureCallbackTime;
 
     public CameraLatency() {
         super("com.android.camera", Camera.class);
@@ -58,11 +69,44 @@ public class CameraLatency extends ActivityInstrumentationTestCase2 <Camera> {
                 inst.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_UP);
                 inst.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_CENTER);
                 Thread.sleep(WAIT_FOR_IMAGE_CAPTURE_TO_BE_TAKEN);
+                //skip the first measurement
+                if (i != 0) {
+                    mTotalAutoFocusTime += Camera.mAutoFocusTime;
+                    mTotalShutterLag += Camera.mShutterLag;
+                    mTotalShutterAndRawPictureCallbackTime +=
+                            Camera.mShutterAndRawPictureCallbackTime;
+                    mTotalJpegPictureCallbackTimeLag += Camera.mJpegPictureCallbackTimeLag;
+                    mTotalRawPictureAndJpegPictureCallbackTime +=
+                            Camera.mRawPictureAndJpegPictureCallbackTime;
+                }
             }
         } catch (Exception e) {
             Log.v(TAG, e.toString());
         }
-            assertTrue("testImageCapture", true);
+        //ToDO: yslau
+        //1) Need to get the baseline from the cupcake so that we can add the
+        //failure condition of the camera latency.
+        //2) Only count those number with succesful capture. Set the timer to invalid
+        //before capture and ignore them if the value is invalid
+        int numberofRun = TOTAL_NUMBER_OF_IMAGECAPTURE - 1;
+        mAvgAutoFocusTime = mTotalAutoFocusTime / numberofRun;
+        mAvgShutterLag = mTotalShutterLag / numberofRun;
+        mAvgShutterAndRawPictureCallbackTime =
+            mTotalShutterAndRawPictureCallbackTime / numberofRun;
+        mAveJpegPictureCallbackTimeLag =
+            mTotalJpegPictureCallbackTimeLag / numberofRun;
+        mAvgRawPictureAndJpegPictureCallbackTime =
+                mTotalRawPictureAndJpegPictureCallbackTime / numberofRun;
+
+        Log.v(TAG, "Avg AutoFocus = " + mAvgAutoFocusTime);
+        Log.v(TAG, "Avg mShutterLag = " + mAvgShutterLag);
+        Log.v(TAG, "Avg mShutterAndRawPictureCallbackTime = "
+                + mAvgShutterAndRawPictureCallbackTime);
+        Log.v(TAG, "Avg mJpegPictureCallbackTimeLag = "
+                + mAveJpegPictureCallbackTimeLag);
+        Log.v(TAG, "Avg mRawPictureAndJpegPictureCallbackTime = "
+                + mAvgRawPictureAndJpegPictureCallbackTime);
+        assertTrue("testImageCapture", true);
     }
 }
 
