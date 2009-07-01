5f2686bef342f62421747fd743cf170d22f0e190
Owen Lin <owenlin@google.com>
2009-06-30 17:17:40 -0700

    Use ReferenceQueue to improve LruCache.

--- src/com/android/camera/gallery/LruCache.java
+++ src/com/android/camera/gallery/LruCache.java
@@ -16,6 +16,7 @@
 
 package com.android.camera.gallery;
 
+import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -25,12 +26,9 @@ import java.util.Map;
 public class LruCache<K, V> {
 
     private final HashMap<K, V> mLruMap;
-    private final HashMap<K, WeakReference<V>> mWeakMap =
-            new HashMap<K, WeakReference<V>>();
-
-    //TODO: use ReferenceQueue to clean the cache
-    private final static int MAXIMUM_ADD_BEFORE_SWEEP = 128;
-    private int mAddBeforeSweep = 0;
+    private final HashMap<K, Entry<K, V>> mWeakMap =
+            new HashMap<K, Entry<K, V>>();
+    private ReferenceQueue<V> mQueue = new ReferenceQueue<V>();
 
     public LruCache(final int capacity) {
         mLruMap = new LinkedHashMap<K, V>(16, 0.75f, true) {
@@ -41,34 +39,42 @@ public class LruCache<K, V> {
         };
     }
 
-    public synchronized V put(K key, V value) {
-        if (++mAddBeforeSweep > MAXIMUM_ADD_BEFORE_SWEEP) {
-            mAddBeforeSweep = 0;
-            Iterator<Map.Entry<K, WeakReference<V>>> iter =
-                    mWeakMap.entrySet().iterator();
-            while (iter.hasNext()) {
-                Map.Entry<K, WeakReference<V>> entry = iter.next();
-                if (entry.getValue().get() == null) iter.remove();
-            }
+    private static class Entry<K, V> extends WeakReference<V> {
+        K mKey;
+
+        public Entry(K key, V value, ReferenceQueue queue) {
+            super(value, queue);
+            mKey = key;
         }
+    }
+
+    private void cleanUpWeakMap() {
+        Entry<K, V> entry = (Entry<K, V>) mQueue.poll();
+        while (entry != null) {
+            mWeakMap.remove(entry.mKey);
+            entry = (Entry<K, V>) mQueue.poll();
+        }
+    }
+
+    public synchronized V put(K key, V value) {
+        cleanUpWeakMap();
         mLruMap.put(key, value);
-        WeakReference<V> ref = mWeakMap.put(key, new WeakReference<V>(value));
-        return ref == null ? null : ref.get();
+        Entry<K, V> entry = mWeakMap.put(
+                key, new Entry<K, V>(key, value, mQueue));
+        return entry == null ? null : entry.get();
     }
 
     public synchronized V get(K key) {
+        cleanUpWeakMap();
         V value = mLruMap.get(key);
         if (value != null) return value;
-        WeakReference<V> ref = mWeakMap.get(key);
-        value = ref == null ? null : ref.get();
-        if (value == null && ref != null) {
-            mWeakMap.remove(key);
-        }
-        return value;
+        Entry<K, V> entry = mWeakMap.get(key);
+        return entry == null ? null : entry.get();
     }
 
     public synchronized void clear() {
         mLruMap.clear();
         mWeakMap.clear();
+        mQueue = new ReferenceQueue<V>();
     }
 }
