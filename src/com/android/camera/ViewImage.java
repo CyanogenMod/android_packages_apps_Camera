b0aadd00e7bb48316fb21369869fad8dd94ace88
Ray Chen <raychen@google.com>
2009-07-21 14:55:35 +0800

    Use revised zoom widget, on the right in Gallery.

--- src/com/android/camera/ViewImage.java
+++ src/com/android/camera/ViewImage.java
@@ -27,6 +27,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -36,16 +37,20 @@ import android.provider.MediaStore;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 import android.widget.ZoomButtonsController;
 
@@ -265,6 +270,15 @@ public class ViewImage extends Activity implements View.OnClickListener {
         mZoomButtonsController = new ZoomButtonsController(rootView);
         mZoomButtonsController.setAutoDismissed(false);
         mZoomButtonsController.setZoomSpeed(100);
+
+        View controls = mZoomButtonsController.getZoomControls();
+        ViewGroup.LayoutParams params = controls.getLayoutParams();
+        if (params instanceof FrameLayout.LayoutParams) {
+            FrameLayout.LayoutParams frameParams =
+                    (FrameLayout.LayoutParams) params;
+            frameParams.gravity = Gravity.RIGHT;
+        }
+
         mZoomButtonsController.setOnZoomListener(
                 new ZoomButtonsController.OnZoomListener() {
             public void onVisibilityChanged(boolean visible) {
