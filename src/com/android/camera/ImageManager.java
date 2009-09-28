140229cc988a264d9719b8dcc41196cc87092440
Chih-Chung Chang <chihchung@google.com>
2009-09-28 10:47:18 -0700

    Fix orientation not stored in the database.

--- src/com/android/camera/ImageManager.java
+++ src/com/android/camera/ImageManager.java
@@ -225,9 +225,16 @@ public class ImageManager {
         cr.update(uri, values, null, null);
     }
 
-    public static Uri addImage(ContentResolver cr, String title,
-            long dateTaken, Location location,
-            int orientation, String directory, String filename, Bitmap source, byte[] jpegData) {
+    //
+    // Stores a bitmap or a jpeg byte array to a file (using the specified
+    // directory and filename). Also add an entry to the media store for
+    // this picture. The title, dateTaken, location are attributes for the
+    // picture. The degree is a one element array which returns the orientation
+    // of the picture.
+    //
+    public static Uri addImage(ContentResolver cr, String title, long dateTaken,
+            Location location, String directory, String filename,
+            Bitmap source, byte[] jpegData, int[] degree) {
         // We should store image data earlier than insert it to ContentProvider, otherwise
         // we may not be able to generate thumbnail in time.
         OutputStream outputStream = null;
@@ -239,8 +246,10 @@ public class ImageManager {
             outputStream = new FileOutputStream(file);
             if (source != null) {
                 source.compress(CompressFormat.JPEG, 75, outputStream);
+                degree[0] = 0;
             } else {
                 outputStream.write(jpegData);
+                degree[0] = getExifOrientation(filePath);
             }
         } catch (FileNotFoundException ex) {
             Log.w(TAG, ex);
@@ -261,7 +270,7 @@ public class ImageManager {
         values.put(Images.Media.DISPLAY_NAME, filename);
         values.put(Images.Media.DATE_TAKEN, dateTaken);
         values.put(Images.Media.MIME_TYPE, "image/jpeg");
-        values.put(Images.Media.ORIENTATION, orientation);
+        values.put(Images.Media.ORIENTATION, degree[0]);
         values.put(Images.Media.DATA, filePath);
 
         if (location != null) {
