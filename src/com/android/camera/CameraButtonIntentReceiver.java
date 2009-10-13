609d8cc0c7d05aa294aeebea8dcbb9111aefc1e1
Cheng-Ru Lin <owenlin@google.com>
2009-10-13 06:22:55 -0700

    Clear task stack when launch camera with the hardware button.
    Bug: http://b/issue?id=2109092
    Change-Id: Ia9b5cb7dfd6abd5c77e34e408cb761b4145bfd3d

--- src/com/android/camera/CameraButtonIntentReceiver.java
+++ src/com/android/camera/CameraButtonIntentReceiver.java
@@ -21,8 +21,6 @@ import android.content.Context;
 import android.content.Intent;
 
 public class CameraButtonIntentReceiver extends BroadcastReceiver {
-    public CameraButtonIntentReceiver() {
-    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -35,8 +33,9 @@ public class CameraButtonIntentReceiver extends BroadcastReceiver {
         holder.release();
         Intent i = new Intent(Intent.ACTION_MAIN);
         i.setClass(context, Camera.class);
-        i.addCategory("android.intent.category.LAUNCHER");
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         context.startActivity(i);
     }
 }
