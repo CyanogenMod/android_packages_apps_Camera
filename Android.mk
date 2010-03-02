aafae1fd1ed72c6849c2594a31e98d72fca49fee
Wu-cheng Li <wuchengli@google.com>
2010-03-02 15:11:52 -0800

    Disable proguard to avoid camera crash.
    We should check what classes/members we want to keep. Before that, let's disable
    it to keep camera working.

--- Android.mk
+++ Android.mk
@@ -8,6 +8,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := Camera
 LOCAL_CERTIFICATE := media
 
+LOCAL_PROGUARD_ENABLED := disabled
+
 include $(BUILD_PACKAGE)
 
 # Use the following include to make our test apk.
