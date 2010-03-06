be69d39c4ab8fa93a52ed13a72ba2ad582adf583
Chih-Chung Chang <chihchung@google.com>
2010-03-05 21:04:51 -0800

    Revert "Unbundle."
    This reverts commit 7319f3f17216969cdb364224d748ef8d3b68af93.

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := Camera
-LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := media
 
 LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard.flags
 
