45cd4afa04c8c7107385a01ae9aefc49c7024d78
Michael Webster <miketwebster@gmail.com>
2010-11-11 21:12:01 -0500

    Simplify Camera smooth-zoom functions for Droid to make it operate more reliably.

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -425,19 +425,15 @@ public class Camera extends BaseCamera {
 
     protected void onZoomValueChanged(int index) {
         if (mSmoothZoomSupported) {
-            if (mTargetZoomValue != index && mZoomState != ZOOM_STOPPED) {
-                mTargetZoomValue = index;
-                if (mZoomState == ZOOM_START) {
-                    mZoomState = ZOOM_STOPPING;
-                    mCameraDevice.stopSmoothZoom();
-                }
-            } else if (mZoomState == ZOOM_STOPPED && mZoomValue != index) {
-                mTargetZoomValue = index;
+            if (mTargetZoomValue != index) {
+                mZoomValue = index;
+                mCameraDevice.stopSmoothZoom();
                 mCameraDevice.startSmoothZoom(index);
-                mZoomState = ZOOM_START;
+                mTargetZoomValue = index;
+            } else {
+                mCameraDevice.stopSmoothZoom();
             }
         } else {
-            mZoomValue = index;
             setCameraParametersWhenIdle(UPDATE_PARAM_ZOOM);
         }
     }
@@ -943,7 +939,7 @@ public class Camera extends BaseCamera {
         FrameLayout frame = (FrameLayout) findViewById(R.id.frame);
         mGLRootView = new GLRootView(this);
         frame.addView(mGLRootView);
-
+        mCameraDevice.stopSmoothZoom();
         mHeadUpDisplay = new CameraHeadUpDisplay(this, mParameters);
         CameraSettings settings = new CameraSettings(this, mInitialParams);
         mHeadUpDisplay.initialize(this,
@@ -1215,7 +1211,7 @@ public class Camera extends BaseCamera {
     @Override
     protected void onResume() {
         super.onResume();
-
+        mCameraDevice.stopSmoothZoom();
         mPausing = false;
         mJpegPictureCallbackTime = 0;
         mZoomValue = 0;
@@ -1514,7 +1510,7 @@ public class Camera extends BaseCamera {
         if (mPreviewing) stopPreview();
         clearFocusState();
         resetFocusIndicator();
-
+        mZoomState = ZOOM_STOPPED;
         setPreviewDisplay(mSurfaceHolder);
         setCameraParameters(UPDATE_PARAM_ALL);
 
@@ -1528,7 +1524,6 @@ public class Camera extends BaseCamera {
             throw new RuntimeException("startPreview failed", ex);
         }
         mPreviewing = true;
-        mZoomState = ZOOM_STOPPED;
         mStatus = IDLE;
 
         /* Get the correct max zoom value, as this varies with
@@ -1796,7 +1791,7 @@ public class Camera extends BaseCamera {
         } else {
             if (!mHandler.hasMessages(SET_CAMERA_PARAMETERS_WHEN_IDLE)) {
                 mHandler.sendEmptyMessageDelayed(
-                        SET_CAMERA_PARAMETERS_WHEN_IDLE, 1000);
+                        SET_CAMERA_PARAMETERS_WHEN_IDLE, 3000);
             }
         }
     }
@@ -1973,6 +1968,7 @@ public class Camera extends BaseCamera {
 
     private boolean switchToVideoMode() {
         if (isFinishing() || !isCameraIdle()) return false;
+        closeCamera();
         MenuHelper.gotoVideoMode(this);
         mHandler.removeMessages(FIRST_TIME_INIT);
         finish();
