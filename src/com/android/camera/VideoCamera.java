265b714056cf005a39360dec163c47b3bb4f4b91
Sang Tae Park <pastime1971@gmail.com>
2011-07-01 14:58:13 -0700

    extends volume button zoom to video mode  * code simplified  * key up sound prevented
    Change-Id: Ie9814a88055588e897dda439a93d401db667bd7d

--- src/com/android/camera/VideoCamera.java
+++ src/com/android/camera/VideoCamera.java
@@ -31,6 +31,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -540,7 +541,6 @@ public class VideoCamera extends BaseCamera
     }
 
     private void onStopVideoRecording(boolean valid) {
-        mHeadUpDisplay.setVideoQualityControlsEnabled(true);
         if (mIsVideoCaptureIntent) {
             if (mQuickCapture) {
                 stopVideoRecordingAndReturn(valid);
@@ -814,6 +814,8 @@ public class VideoCamera extends BaseCamera
             return true;
         }
 
+        SharedPreferences prefs = getSharedPreferences("com.android.camera_preferences", 0);
+        boolean volZoom = prefs.getBoolean("vol_zoom_enabled", false);
         switch (keyCode) {
             case KeyEvent.KEYCODE_CAMERA:
                 if (event.getRepeatCount() == 0) {
@@ -830,6 +832,19 @@ public class VideoCamera extends BaseCamera
             case KeyEvent.KEYCODE_MENU:
                 if (mMediaRecorderRecording) {
                     onStopVideoRecording(true);
+                    return true;
+                }
+                break;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if (volZoom) {
+                    if (keyCode == KeyEvent.KEYCODE_VOLUME_UP && mZoomValue < mZoomMax) {
+                        mZoomValue ++;
+                    } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN && mZoomValue > 0) {
+                        mZoomValue--;
+                    }
+                    onZoomValueChanged(mZoomValue);
+                    mHeadUpDisplay.setZoomIndex(mZoomValue);
                     return true;
                 }
                 break;
