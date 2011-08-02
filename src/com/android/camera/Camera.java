bf12860a93d8376c7964583578e9eb30a945498b
Pawit Pornkitprasan <p.pawit@gmail.com>
2011-08-02 21:14:30 +0700

    Continuous autofocus for samsung phones (galaxysmtd, galaxys2) (1/3)
    Samsung has several hidden settings in its camera parameters.
    
     - 'video_recording_gamma' and 'slow_ae' are disabled in photo mode
       and enabled in video mode
     - 'iso' is always set to 'movie' in video mode
     - 'metering' is always set to 'matrix' in video mode
     - 'continuous_af' is for turning on continuous autofocus while
       taking a video
    
    Change-Id: I97f4ad229e2866773b02137376f30aa8ee9487ea

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -1829,6 +1829,9 @@ public class Camera extends BaseCamera implements View.OnClickListener,
         Util.setCameraDisplayOrientation(this, mCameraId, mCameraDevice);
         setCameraParameters(UPDATE_PARAM_ALL);
 
+        CameraSettings.setVideoMode(mParameters, false);
+        mCameraDevice.setParameters(mParameters);
+
         mCameraDevice.setErrorCallback(mErrorCallback);
 
         try {
@@ -1847,8 +1850,6 @@ public class Camera extends BaseCamera implements View.OnClickListener,
         */
         mParameters = mCameraDevice.getParameters();
         mZoomMax = mParameters.getMaxZoom();
-        CameraSettings.setVideoMode(mParameters, false);
-        mCameraDevice.setParameters(mParameters);
     }
 
     private void stopPreview() {
