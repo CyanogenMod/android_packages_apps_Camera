46521167db54dcc88e5bf6dc82798fa912a5a66a
Wu-cheng Li <wuchengli@google.com>
2011-08-18 17:14:18 +0800

    Update white-balance and remove old setting icon.
    bug:5024054
    Change-Id: I6a11ba498c2eee82f5496594b61351fe745eb4a9

--- src/com/android/camera/ui/IndicatorControl.java
+++ src/com/android/camera/ui/IndicatorControl.java
@@ -91,7 +91,7 @@ public abstract class IndicatorControl extends RelativeLayout implements
         mPreferenceGroup = group;
         // Add other settings indicator.
         if (otherSettingKeys != null) {
-            addOtherSettingIndicator(context, R.drawable.ic_viewfinder_settings, otherSettingKeys);
+            addOtherSettingIndicator(context, R.drawable.ic_settings_holo_light, otherSettingKeys);
         }
 
         for (int i = 0; i < keys.length; i++) {
