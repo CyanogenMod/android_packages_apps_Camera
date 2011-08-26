8432fe02f8d14472304de71942b93da73fa89756
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-08-26 20:50:31 +0200

    Camera support for 16:9 devices (From CyanogenDefy)
    Require Wide media profile : http://review.cyanogenmod.com/7317
    
    Change-Id: I964d51c98e894349d83e0e3b46ced1c2fb4f61a2

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -1054,7 +1054,7 @@ public class Camera extends BaseCamera implements View.OnClickListener,
         if (mParameters.isZoomSupported()) {
             ((CameraHeadUpDisplay)mHeadUpDisplay).setZoomIndex(mZoomValue);
         }
-        FrameLayout frame = (FrameLayout) findViewById(R.id.frame);
+        FrameLayout frame = (FrameLayout) findViewById(R.id.framegl);
         mGLRootView = new GLRootView(this);
         mGLRootView.setContentPane(mHeadUpDisplay);
         frame.addView(mGLRootView);
