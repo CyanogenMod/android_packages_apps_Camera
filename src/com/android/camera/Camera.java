db3556078a75be0bd4a08195d90f060724034e88
Chung-yih Wang <cywang@google.com>
2011-09-19 17:49:19 +0800

    Fix indicator bar height in video mode.
    bug:5287560
    Change-Id: I000d11aa3974e1d48d18f4dcf6ec31166f6ef8ec

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -145,6 +145,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
     private boolean mOpenCameraFail = false;
     private boolean mCameraDisabled = false;
 
+    private View mPreviewPanel;  // The container of PreviewFrameLayout.
     private PreviewFrameLayout mPreviewFrameLayout;
     private View mPreviewFrame;  // Preview frame area.
 
@@ -1605,7 +1606,8 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         // Set the preview frame aspect ratio according to the picture size.
         Size size = mParameters.getPictureSize();
 
-        mPreviewFrameLayout = (PreviewFrameLayout) findViewById(R.id.frame_layout);
+        mPreviewPanel = findViewById(R.id.frame_layout);
+        mPreviewFrameLayout = (PreviewFrameLayout) findViewById(R.id.frame);
         mPreviewFrameLayout.setAspectRatio((double) size.width / size.height);
 
         // Set a preview size that is closest to the viewfinder height and has
@@ -1953,8 +1955,9 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
     private void showSharePopup() {
         Uri uri = mThumbnail.getUri();
         if (mSharePopup == null || !uri.equals(mSharePopup.getUri())) {
+            // SharePopup window takes the mPreviewPanel as its size reference.
             mSharePopup = new SharePopup(this, uri, mThumbnail.getBitmap(),
-                    mOrientationCompensation, mPreviewFrameLayout);
+                    mOrientationCompensation, mPreviewPanel);
         }
         mSharePopup.showAtLocation(mThumbnailView, Gravity.NO_GRAVITY, 0, 0);
     }
