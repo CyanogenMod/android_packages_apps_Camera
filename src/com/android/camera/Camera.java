1db20da2e9aec870daec218db5183f117d5ac558
Valera Trubachev <vetruvet@gmail.com>
2011-10-27 19:32:00 -0400

    Camera: Allow focus sound muting (3/3)
    Allows muting of focus sound for silent camera operation
    if shutter is also muted.
    
    Change-Id: I1b57c6b5986e11b80546b30d94c602e6c7b0e336

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -228,6 +228,8 @@ public class Camera extends BaseCamera implements View.OnClickListener,
     private int mImageWidth = 0;
     private int mImageHeight = 0;
 
+    private boolean mFocusMute = false;
+
     /**
      * This Handler is used to post message back onto the main thread of the
      * application
@@ -300,6 +302,9 @@ public class Camera extends BaseCamera implements View.OnClickListener,
     // make preview screen appear as soon as possible.
     private void initializeFirstTime() {
         if (mFirstTimeInitialized) return;
+
+        mFocusMute = (Settings.System.getInt(getContentResolver(),
+                Settings.System.CAMERA_FOCUS_MUTE, 0) == 1);
 
         // Create orientation listenter. This should be done first because it
         // takes some time to get first orientation.
@@ -688,7 +693,7 @@ public class Camera extends BaseCamera implements View.OnClickListener,
                 // User is half-pressing the focus key. Play the focus tone.
                 // Do not take the picture now.
                 ToneGenerator tg = mFocusToneGenerator;
-                if (tg != null) {
+                if (tg != null && !mFocusMute) {
                     tg.startTone(ToneGenerator.TONE_PROP_BEEP2);
                 }
                 if (focused) {
@@ -1380,6 +1385,9 @@ public class Camera extends BaseCamera implements View.OnClickListener,
         mJpegPictureCallbackTime = 0;
         mZoomValue = 0;
         mImageCapture = new ImageCapture();
+
+        mFocusMute = (Settings.System.getInt(getContentResolver(),
+                Settings.System.CAMERA_FOCUS_MUTE, 0) == 1);
 
         // Start the preview if it is not started.
         if (!mPreviewing && !mStartPreviewFail) {
