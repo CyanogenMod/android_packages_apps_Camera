bc581feb211f3d10e73bdd7deb78cfc42a09e245
Greg Kochaniak <greg@kochaniak.com>
2011-12-12 17:19:38 -0500

    Added option to use either removable SD card or internal memory
    On the "Advanced options" screen, added the option "Use external SD card",
    with description "Store photos and videos on external (removable) SD card"
    is enabled if a phone has both internal memory and removable SD card, and
    the external card is mounted and writable. Otherwise this option is
    disabled.  Takes into account the Cyanogen "persist.sys.vold.switchexternal"
    property to detect which of /mnt/sdcard and /mnt/emmc is the true removable
    SD card and makes the Camera save photos and videos to /DCIM directory there.
    
    The default is as per prior Cyanogen setting, i.e.  use the same storage
    as the main programs and data storage, but now can be easily switched if
    the user runs out of memory while recording photos and videos, or can keep
    all programs and data on internal phone storage, while swapping SD cards
    as needed for recording more photos and videos.
    
    Dec. 22, 2011 (patch set 11) - fixed whitespace issues.
    
    Dec. 21, 2011 (patch set 10) - commiting changes made by Danny, but there
    are issues now making the patch unusable if CMParts "Use internal storage"
    option is off, plus performance issues. Discussing further in review.
    
    Dec. 20, 2011 (patch set 8, 9) - more whitespace fixes.
    
    Dec. 20, 2011 (patch set 7) - implemented code changes suggested by
    reviewer Danny Baumann - thank you, Danny, for excellent suggestions!
    
    Dec. 17, 2011 (patch set 6) - Taichi verified that no more FC on passion
    and crespo, removing debugging Log.d() lines from ImageManager.java
    
    Dec. 16, 2011 - replaced multiple exceptions in ImageManager.java -
    sysProps_GetInt() with one. Fixed minor formatting issues as advised by
    code reviewers.
    
    Dec. 14, 2011 - some cleanup in ImageManager.java plus added Log.d()
    lines to debug the reported FC on passion and crespo.
    
    Change-Id: Ibef2304640dfd7c060927d4268a10af1c388500a

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -49,7 +49,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.MessageQueue;
-import android.preference.CheckBoxPreference;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.util.Log;
@@ -761,7 +760,8 @@ public class Camera extends BaseCamera implements View.OnClickListener,
                         title,
                         dateTaken,
                         loc, // location from gps/network
-                        ImageManager.CAMERA_IMAGE_BUCKET_NAME, filename,
+                        ImageManager.getCameraImageDirectory(),
+                        filename,
                         null, data,
                         degree);
                 return degree[0];
@@ -1136,6 +1136,7 @@ public class Camera extends BaseCamera implements View.OnClickListener,
     }
 
     private void checkStorage() {
+        ImageManager.updateStorageDirectory(this);
         calculatePicturesRemaining();
         updateStorageHint(mPicturesRemaining);
     }
@@ -1832,7 +1833,7 @@ public class Camera extends BaseCamera implements View.OnClickListener,
             dataLocation(),
             ImageManager.INCLUDE_IMAGES,
             ImageManager.SORT_ASCENDING,
-            ImageManager.CAMERA_IMAGE_BUCKET_ID);
+            ImageManager.getCameraImageBucketId());
         int count = list.getCount();
         if (count > 0) {
             IImage image = list.getImageAt(count - 1);
@@ -2321,7 +2322,7 @@ public class Camera extends BaseCamera implements View.OnClickListener,
     }
 
     private int calculatePicturesRemaining() {
-        mPicturesRemaining = MenuHelper.calculatePicturesRemaining();
+        mPicturesRemaining = MenuHelper.calculatePicturesRemaining(this);
         return mPicturesRemaining;
     }
 
