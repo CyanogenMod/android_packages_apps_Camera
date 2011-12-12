12345c82f76ec7ebd25af19f96588f2632dff725
Greg Kochaniak <greg@kochaniak.com>
2011-12-12 17:19:38 -0500

    Added option to use either removable SD card or internal memory
    On the "Advanced options" screen, added the option "Use external SD card",
    with description "Store photos and videos on external (removable) SD card"
    is enabled if a phone has both internal memory and removable SD card, and
    the external card is mounted and writable. Otherwise this option is
    disabled.  Takes into account the Cyanogen "persist.sys.vold.switchexternal"
    property to detect which of /mnt/sdcard and /mnt/emmc is the true removable
    SD card and makes the Camera save photos and videos to /DCIM directory there.
    
    The default is as per prior Cyanogen setting, i.e.  use the same storage
    as the main programs and data storage, but now can be easily switched if
    the user runs out of memory while recording photos and videos, or can keep
    all programs and data on internal phone storage, while swapping SD cards
    as needed for recording more photos and videos.
    
    Dec. 16, 2011 - replaced multiple exceptions in ImageManager.java -
    sysProps_GetInt() with one. Fixed minor formatting issues as advised by
    code reviewers.
    
    Dec. 14, 2011 - some cleanup in ImageManager.java plus added Log.d()
    lines to debug the reported FC on passion and crespo.
    
    (Please note: this change does not depend on I63d4345e as review site
    insists, but don't know how to remove this note.)
    
    Change-Id: Ibef2304640dfd7c060927d4268a10af1c388500a

--- src/com/android/camera/ImageManager.java
+++ src/com/android/camera/ImageManager.java
@@ -47,6 +47,10 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+
 /**
  * {@code ImageManager} is used to retrieve and store images
  * in the media content provider.
@@ -61,6 +65,113 @@ public class ImageManager {
     private ImageManager() {
     }
 
+    // The mess below is due to all methods in android.os.SystemProperties being
+    // hidden, even as trivial method as GetInt(name, defVal). Have to use Java
+    // reflection to read "persist.sys.vold.switchexternal" Cyanogen setting.
+    private static int sysProps_GetInt(String name, int defVal) {
+        int retVal = defVal;
+        try {
+            Class<?> c = Class.forName("android.os.SystemProperties");
+            Class<?> partypes[] = new Class[2];
+            partypes[0] = String.class;
+            partypes[1] = Integer.TYPE;
+            Method SysProp_getInt = c.getDeclaredMethod("getInt", partypes);
+            Object[] argObjects = { name, defVal };
+            retVal = ((Integer) SysProp_getInt.invoke(null, argObjects))
+                    .intValue();
+            
+        } catch (Exception x) {
+            // Possible exceptions are: NoSuchMethodException, ClassNotFoundException
+            // IllegalAccessException, IllegalArgumentException, 
+            // InvocationTargetException.
+            // They are rather unlikely to happen, and if they do, we just 
+            // continue with the Camera default setup.
+            Log.d("CamImgMgr", "sysProps_GetInt() exception: " + x.toString());
+        }
+
+        return retVal;
+    }
+
+    private static String removableDir;
+    private static String internalDir;
+    private static boolean isSwitchedExternal;
+
+    static {
+        Log.d("CamImgMgr", "Calling sysProps_GetInt()...");
+        isSwitchedExternal = (sysProps_GetInt("persist.sys.vold.switchexternal", 0) == 1);
+        if (isSwitchedExternal)
+            Log.d("CamImgMgr", "isSwitchedExternal is true");
+        else
+            Log.d("CamImgMgr", "isSwitchedExternal is false");
+
+        internalDir = System.getenv("PHONE_STORAGE");  // usually /mnt/emmc on Cyanogen
+        if (internalDir == null) {
+            Log.d("CamImgMgr", "internalDir is NULL.");
+            internalDir = "/mnt/emmc";
+        } else
+            Log.d("CamImgMgr", "internalDir=" + internalDir);
+
+        removableDir = Environment.getExternalStorageDirectory().toString(); // usually /mnt/sdcard on Cyanogen
+        Log.d("CamImgMgr", "removableDir=" + removableDir);
+
+        if (isSwitchedExternal) {
+            String s = internalDir;
+            internalDir = removableDir;
+            removableDir = s;
+        } 
+
+        Log.d("CamImgMgr", "checkFsWritable " + internalDir);
+        if (!checkFsWritable(internalDir))
+            internalDir = "";
+        Log.d("CamImgMgr", "checkFsWritable " + removableDir);
+        if (!checkFsWritable(removableDir))
+            removableDir = "";
+        Log.d("CamImgMgr", "Final: internalDir=" + internalDir);
+        Log.d("CamImgMgr", "Final: removableDir=" + removableDir);
+    }
+
+    public static boolean enableExtSdOption() {
+        return !"".equals(removableDir) && !"".equals(internalDir) && !removableDir.equals(internalDir);
+    }
+
+    public static boolean mUseRemovableMem = !isSwitchedExternal;
+
+    public static String getStorageDirectory() {
+        String s;
+        //Log.d("CamImgMgr", "getStorageDirectory 1, mUseRemovableMem=" + mUseRemovableMem);
+        if (mUseRemovableMem && !"".equals(removableDir))
+            s = removableDir;
+        else if (!"".equals(internalDir))
+            s = internalDir;
+        else
+            s = Environment.getExternalStorageDirectory().toString();
+        //Log.d("CamImgMgr", "getStorageDirectory() return s=" + s);
+        return s;
+    }
+
+    /**
+     * Matches code in MediaProvider.computeBucketValues. Should be a common
+     * function.
+     */
+    public static String getBucketId(String path) {
+        return String.valueOf(path.toLowerCase().hashCode());
+    }
+
+    public static String getCameraImageDirectory() {
+        return getStorageDirectory() + "/DCIM/Camera";
+    }
+
+    public static String getCameraImageBucketId() {
+        return getBucketId(getCameraImageDirectory());
+    }
+
+    public static final String GALLERY_IMAGE_BUCKET_NAME =
+            Environment.getExternalStorageDirectory().toString()
+            + "/DCIM/Camera";
+    public static final String GALLERY_IMAGE_BUCKET_ID =
+            getBucketId(GALLERY_IMAGE_BUCKET_NAME);
+    
+ 	
     /**
      * {@code ImageListParam} specifies all the parameters we need to create an
      * image list (we also need a ContentResolver).
@@ -127,28 +238,13 @@ public class ImageManager {
     public static final int SORT_ASCENDING = 1;
     public static final int SORT_DESCENDING = 2;
 
-    public static final String CAMERA_IMAGE_BUCKET_NAME =
-            Environment.getExternalStorageDirectory().toString()
-            + "/DCIM/Camera";
-    public static final String CAMERA_IMAGE_BUCKET_ID =
-            getBucketId(CAMERA_IMAGE_BUCKET_NAME);
-
-    /**
-     * Matches code in MediaProvider.computeBucketValues. Should be a common
-     * function.
-     */
-    public static String getBucketId(String path) {
-        return String.valueOf(path.toLowerCase().hashCode());
-    }
-
     /**
      * OSX requires plugged-in USB storage to have path /DCIM/NNNAAAAA to be
      * imported. This is a temporary fix for bug#1655552.
      */
     public static void ensureOSXCompatibleFolder() {
         File nnnAAAAA = new File(
-            Environment.getExternalStorageDirectory().toString()
-            + "/DCIM/100ANDRO");
+        	getStorageDirectory() + "/DCIM/100ANDRO");
         if ((!nnnAAAAA.exists()) && (!nnnAAAAA.mkdir())) {
             Log.e(TAG, "create NNNAAAAA file: " + nnnAAAAA.getPath()
                     + " failed");
@@ -330,20 +426,29 @@ public class ImageManager {
         return makeImageList(cr, param);
     }
 
-    private static boolean checkFsWritable() {
+    private static boolean checkFsWritable(String directoryName) {
         // Create a temporary file to see whether a volume is really writeable.
         // It's important not to put it in the root directory which may have a
         // limit on the number of files.
-        String directoryName =
-                Environment.getExternalStorageDirectory().toString() + "/DCIM";
+    	if ("".equals(directoryName))
+    		return false;
+    	boolean created = false;
+    	directoryName += "/DCIM";
         File directory = new File(directoryName);
         if (!directory.isDirectory()) {
             if (!directory.mkdirs()) {
                 return false;
             }
+            created = true;
         }
-        return directory.canWrite();
+        boolean ret = directory.canWrite();
+        if (created)
+        	directory.delete();
+        return ret;
     }
+    
+    private static boolean checkFsWritable() {
+    	return checkFsWritable(getStorageDirectory());    }
 
     public static boolean hasStorage() {
         return hasStorage(true);
