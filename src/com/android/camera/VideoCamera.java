daedc61ece02d3bf2c023ea8a0a2bf1f2b1d9e40
Greg Kochaniak <greg@kochaniak.com>
2011-12-12 17:04:07 -0500

    Revert "Camera - Advanced options - adding option to use either the removable SD card, or internal memory. Disabled if removable SD not present or not writable. Takes into account the Cyanogen "persist.sys.vold.switchexternal" property to detect which of /mnt/sdcard and /mnt/emmc is the true removable SD card and makes the Camera save photos and videos to /DCIM directory there."
    This reverts commit 2e5f58be63bd09fd64ff947c493166fbeb7dca4c. Some of my
    changes did not make it into the original commit, plus a better
    formatted description is needed

--- src/com/android/camera/VideoCamera.java
+++ src/com/android/camera/VideoCamera.java
@@ -1007,7 +1007,7 @@ public class VideoCamera extends BaseCamera
                 return NO_STORAGE_ERROR;
             } else {
                 String storageDirectory =
-                		ImageManager.getStorageDirectory();
+                        Environment.getExternalStorageDirectory().toString();
                 StatFs stat = new StatFs(storageDirectory);
                 return (long) stat.getAvailableBlocks()
                         * (long) stat.getBlockSize();
@@ -1157,7 +1157,7 @@ public class VideoCamera extends BaseCamera
         String title = createName(dateTaken);
         String filename = title + 
             (MediaRecorder.OutputFormat.MPEG_4 == mProfile.fileFormat ? ".m4v" : ".3gp"); // Used when emailing.
-        String cameraDirPath = ImageManager.getCameraImageDirectory();
+        String cameraDirPath = ImageManager.CAMERA_IMAGE_BUCKET_NAME;
         String filePath = cameraDirPath + "/" + filename;
         File cameraDir = new File(cameraDirPath);
         cameraDir.mkdirs();
@@ -1535,7 +1535,7 @@ public class VideoCamera extends BaseCamera
                         dataLocation(),
                         ImageManager.INCLUDE_VIDEOS,
                         ImageManager.SORT_ASCENDING,
-                        ImageManager.getCameraImageBucketId());
+                        ImageManager.CAMERA_IMAGE_BUCKET_ID);
         int count = list.getCount();
         if (count > 0) {
             IImage image = list.getImageAt(count - 1);
