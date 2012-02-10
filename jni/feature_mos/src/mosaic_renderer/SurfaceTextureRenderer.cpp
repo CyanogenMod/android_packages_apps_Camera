d3613d6cfc27a2ed150b5845fc672624484c4f12
Ricardo Cerqueira <github@cerqueira.org>
2012-02-10 11:51:32 +0000

    mosaic: Hack renderer to support devices without external textures
    Change-Id: I01511705d3452ccaa4f761e95c9e71e236b1da77

--- jni/feature_mos/src/mosaic_renderer/SurfaceTextureRenderer.cpp
+++ jni/feature_mos/src/mosaic_renderer/SurfaceTextureRenderer.cpp
@@ -29,7 +29,11 @@ const GLfloat g_vVertices[] = {
 };
 GLushort g_iIndices2[] = { 0, 1, 2, 3 };
 
+#ifdef MISSING_EGL_EXTERNAL_IMAGE
+const int GL_TEXTURE_EXTERNAL_OES_ENUM = 0x0DE1;
+#else
 const int GL_TEXTURE_EXTERNAL_OES_ENUM = 0x8D65;
+#endif
 
 const int VERTEX_STRIDE = 6 * sizeof(GLfloat);
 
@@ -177,7 +181,11 @@ const char* SurfaceTextureRenderer::FragmentShaderSource() const
         "#extension GL_OES_EGL_image_external : require\n"
         "precision mediump float;\n"
         "varying vec2 vTextureNormCoord;\n"
+#ifdef MISSING_EGL_EXTERNAL_IMAGE
+        "uniform sampler2D sTexture;\n"
+#else
         "uniform samplerExternalOES sTexture;\n"
+#endif
         "void main() {\n"
         "  gl_FragColor = texture2D(sTexture, vTextureNormCoord);\n"
         "}\n";
