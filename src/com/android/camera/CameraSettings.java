b3a51d84b43ae507f132370271658431396b89e6
XpLoDWilD <xplodgui@gmail.com>
2012-02-10 17:28:47 +0000

    Add Samsung camera settings
    Change-Id: Idb5da2ce57f8339538c364a650f36c598e9e02a6

--- src/com/android/camera/CameraSettings.java
+++ src/com/android/camera/CameraSettings.java
@@ -70,6 +70,7 @@ public class CameraSettings {
 
     // Samsung camcorder mode
     private static boolean mSamsungCamMode;
+    private static boolean mSamsungCamSettings;
 
     public CameraSettings(Activity activity, Parameters parameters,
                           int cameraId, CameraInfo[] cameraInfo) {
@@ -79,7 +80,7 @@ public class CameraSettings {
         mCameraInfo = cameraInfo;
 
         mSamsungCamMode = mContext.getResources().getBoolean(R.bool.needsSamsungCamMode);
-
+        mSamsungCamSettings = mContext.getResources().getBoolean(R.bool.hasSamsungCamSettings);
     }
 
     public PreferenceGroup getPreferenceGroup(int preferenceRes) {
@@ -180,8 +181,8 @@ public class CameraSettings {
             if (mParameters.getMaxNumFocusAreas() == 0) {
                 filterUnsupportedOptions(group,
                         focusMode, mParameters.getSupportedFocusModes());
-            } else {
-                // Remove the focus mode if we can use tap-to-focus.
+            } else if(!mSamsungCamSettings) {
+                // Remove the focus mode if we can use tap-to-focus
                 removePreference(group, focusMode.getKey());
             }
         }
