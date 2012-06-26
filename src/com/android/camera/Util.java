63d7e03ad964d76538bc2066dd062a4df539dc49
Alexandre Dumont <adumont@gmail.com>
2012-06-27 01:27:40 +0200

    Implement rotation compensation for landscape-always devices
    Adjusts the pictures/videos according to the valor of the config integer
    deviceScreenRotation (value in 0, 90, 180, 270).
    
    Example of use: HTC Chacha, device with always fixed open keyboard and landscape screen.
    Without the use of that property, all pictures and videos taken are rotated 90 degrees.
    Setting deviceScreenRotation to 90 will correct the issue.
    
    Corresponding patch merged in CM7 was Change-Id: I04f3d81a0d96b6e7c98121b1fa6fe88975580f97
    
    Change-Id: I68377e0a71f40a9a589ab1bfce0204e2d61d26ce

--- src/com/android/camera/Util.java
+++ src/com/android/camera/Util.java
@@ -93,6 +93,9 @@ public class Util {
     private static boolean sSamsungCamMode;
     private static boolean sSamsungCamSettings;
 
+    // Enables to compensate the screen rotation
+    private static int sDeviceScreenRotation;
+
     private Util() {
     }
 
@@ -111,6 +114,8 @@ public class Util {
         sEarlyVideoSize = context.getResources().getBoolean(R.bool.needsEarlyVideoSize);
         sSamsungCamMode = context.getResources().getBoolean(R.bool.needsSamsungCamMode);
         sSamsungCamSettings = context.getResources().getBoolean(R.bool.hasSamsungCamSettings);
+
+        sDeviceScreenRotation = context.getResources().getInteger(R.integer.deviceScreenRotation);
 
     }
 
@@ -367,10 +372,10 @@ public class Util {
         Camera.getCameraInfo(cameraId, info);
         int result;
         if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-            result = (info.orientation + degrees) % 360;
+            result = (info.orientation + degrees + sDeviceScreenRotation ) % 360;
             result = (360 - result) % 360;  // compensate the mirror
         } else {  // back-facing
-            result = (info.orientation - degrees + 360) % 360;
+            result = (info.orientation - degrees - sDeviceScreenRotation + 360) % 360;
         }
         return result;
     }
@@ -647,9 +652,9 @@ public class Util {
         if (orientation != OrientationEventListener.ORIENTATION_UNKNOWN) {
             CameraInfo info = CameraHolder.instance().getCameraInfo()[cameraId];
             if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
-                rotation = (info.orientation - orientation + 360) % 360;
+                rotation = (info.orientation - orientation + sDeviceScreenRotation + 360) % 360;
             } else {  // back-facing camera
-                rotation = (info.orientation + orientation) % 360;
+                rotation = (info.orientation + orientation - sDeviceScreenRotation ) % 360;
             }
         }
         parameters.setRotation(rotation);
