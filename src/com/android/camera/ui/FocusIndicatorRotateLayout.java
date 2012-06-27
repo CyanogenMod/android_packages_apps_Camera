47a563b623a468f736779da7e21ef69665fc5b80
Chih-Yu Huang <akahuang@google.com>
2012-06-27 11:39:21 +0800

    Use android.view.animation to replace ViewPropertyAnimator.
    bug:6698378
    
    Change-Id: Icb02cac7b9e2e5ca4e4e8fdf8dfdf498a841913c

--- src/com/android/camera/ui/FocusIndicatorRotateLayout.java
+++ src/com/android/camera/ui/FocusIndicatorRotateLayout.java
@@ -16,6 +16,8 @@
 
 package com.android.camera.ui;
 
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.content.Context;
 import android.util.AttributeSet;
 
@@ -27,15 +29,17 @@ public class FocusIndicatorRotateLayout extends RotateLayout implements FocusInd
     // These states are used to make sure the animation is run for at least some
     // time.
     private int mState;
+    private ScaleAnimation mAnimation = new ScaleAnimation();
     private static final int STATE_IDLE = 0;
     private static final int STATE_FOCUSING = 1;
     private static final int STATE_FINISHING = 2;
 
     private Runnable mDisappear = new Disappear();
-    private Runnable mEndAction = new EndAction();
+    private Animation.AnimationListener mEndAction = new EndAction();
     private static final int SCALING_UP_TIME = 1000;
     private static final int SCALING_DOWN_TIME = 200;
     private static final int DISAPPEAR_TIMEOUT = 200;
+    private static final float MAX_SCALE = 1.5f;
 
     public FocusIndicatorRotateLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -48,9 +52,8 @@ public class FocusIndicatorRotateLayout extends RotateLayout implements FocusInd
     @Override
     public void showStart() {
         if (mState == STATE_IDLE) {
-            setDrawable(R.drawable.ic_focus_focusing);
-            animate().withLayer().setDuration(SCALING_UP_TIME)
-                    .scaleX(1.5f).scaleY(1.5f);
+            startAnimation(R.drawable.ic_focus_focusing, SCALING_UP_TIME,
+                    false, 1f, MAX_SCALE);
             mState = STATE_FOCUSING;
         }
     }
@@ -58,9 +61,8 @@ public class FocusIndicatorRotateLayout extends RotateLayout implements FocusInd
     @Override
     public void showSuccess(boolean timeout) {
         if (mState == STATE_FOCUSING) {
-            setDrawable(R.drawable.ic_focus_focused);
-            animate().withLayer().setDuration(SCALING_DOWN_TIME).scaleX(1f)
-                    .scaleY(1f).withEndAction(timeout ? mEndAction : null);
+            startAnimation(R.drawable.ic_focus_focused, SCALING_DOWN_TIME,
+                    timeout, 1f);
             mState = STATE_FINISHING;
         }
     }
@@ -68,27 +70,51 @@ public class FocusIndicatorRotateLayout extends RotateLayout implements FocusInd
     @Override
     public void showFail(boolean timeout) {
         if (mState == STATE_FOCUSING) {
-            setDrawable(R.drawable.ic_focus_failed);
-            animate().withLayer().setDuration(SCALING_DOWN_TIME).scaleX(1f)
-                    .scaleY(1f).withEndAction(timeout ? mEndAction : null);
+            startAnimation(R.drawable.ic_focus_failed, SCALING_DOWN_TIME,
+                    timeout, 1f);
             mState = STATE_FINISHING;
         }
     }
 
     @Override
     public void clear() {
-        animate().cancel();
+        mAnimation.cancel();
         removeCallbacks(mDisappear);
         mDisappear.run();
         setScaleX(1f);
         setScaleY(1f);
     }
 
-    private class EndAction implements Runnable {
+    private void startAnimation(int resid, long duration, boolean timeout,
+            float toScale) {
+        startAnimation(resid, duration, timeout, mAnimation.getCurrentScale(),
+                toScale);
+    }
+
+    private void startAnimation(int resid, long duration, boolean timeout,
+            float fromScale, float toScale) {
+        mChild.setBackgroundDrawable(getResources().getDrawable(resid));
+        mAnimation.cancel();
+        mAnimation.reset();
+        mAnimation.setDuration(duration);
+        mAnimation.setScale(fromScale, toScale);
+        mAnimation.setAnimationListener(timeout ? mEndAction : null);
+        startAnimation(mAnimation);
+    }
+
+    private class EndAction implements Animation.AnimationListener {
         @Override
-        public void run() {
+        public void onAnimationEnd(Animation animation) {
             // Keep the focus indicator for some time.
             postDelayed(mDisappear, DISAPPEAR_TIMEOUT);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+        }
+
+        @Override
+        public void onAnimationStart(Animation animation) {
         }
     }
 
@@ -97,6 +123,32 @@ public class FocusIndicatorRotateLayout extends RotateLayout implements FocusInd
         public void run() {
             mChild.setBackgroundDrawable(null);
             mState = STATE_IDLE;
+        }
+    }
+
+    private class ScaleAnimation extends Animation {
+        private float mFrom = 1f;
+        private float mTo = 1f;
+        private float mCurrentScale = 1f;
+
+        public ScaleAnimation() {
+            setFillAfter(true);
+        }
+
+        public void setScale(float from, float to) {
+            mFrom = from;
+            mTo = to;
+        }
+
+        public float getCurrentScale() {
+            return mCurrentScale;
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            mCurrentScale = mFrom + (mTo - mFrom) * interpolatedTime;
+            t.getMatrix().setScale(mCurrentScale, mCurrentScale,
+                    getWidth() / 2f, getHeight() / 2f);
         }
     }
 }
