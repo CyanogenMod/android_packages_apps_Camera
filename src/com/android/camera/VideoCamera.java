644384a979ec9bd6b237e9031d5afbf592d3056b
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-07-01 18:03:42 +0200

    videocamera: start preview on checkQualityAndStartPreview() in any case
    preview gets stopped before checkQualityAndStartPreview() gets called.
    make sure to restart it on any case otherwise the camera app will hang.
    
    Thanks to TheUnkn0wn (XDA) for finding this.
    
    Change-Id: If01519d16c6802b8b17fb5034102a806f755db2b

--- src/com/android/camera/VideoCamera.java
+++ src/com/android/camera/VideoCamera.java
@@ -2253,10 +2253,9 @@ public class VideoCamera extends ActivityBase
         if (size.width != mDesiredPreviewWidth
                 || size.height != mDesiredPreviewHeight) {
             resizeForPreviewAspectRatio();
-        } else {
-            // Start up preview again
-            startPreview();
         }
+        // Start up preview
+        startPreview();
     }
 
     private void showTimeLapseUI(boolean enable) {
