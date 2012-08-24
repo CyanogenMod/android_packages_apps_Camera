16b9b13d50901224a129aed99a593f2a1c6d6175
Steve Kondik <shade@chemlab.org>
2012-08-23 17:16:42 -0700

    camera: Remove the rotation parameter for video
     * It's not needed for video, and actually causes buffer allocation
       issues with some buggy drivers. It's only ever set for the video
       snapshot case by VideoCamera, but it can be left over from a previous
       camera snapshot.
    
    Change-Id: I3adc3000b53083c60ec213f6f7d6cc6cc0adaa6c

--- src/com/android/camera/VideoCamera.java
+++ src/com/android/camera/VideoCamera.java
@@ -1803,6 +1803,10 @@ public class VideoCamera extends ActivityBase
     private void setCameraParameters() {
         mParameters = mCameraDevice.getParameters();
 
+        // Reset rotation. It's only meant to be used for snapshots, and it causes problems with
+        // some camera drivers if it's left over from a snap or video snap.
+        mParameters.setRotation(0);
+
         // Set video mode
         CameraSettings.setVideoMode(mParameters, true);
 
