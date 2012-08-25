CyanogenMod/android_packages_apps_Camera

commit 61b3662ccce0beacacd5e404dd1b21300a263605

Camera: Fixes stretched and shrunk preview issue on camera
Change-Id: Ief87c107786eb1be39d3ec7383efbfafc060fedb

src/com/android/camera/Camera.java
@@ -127,7 +127,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
     private static final int UPDATE_PARAM_PREFERENCE = 4;
     private static final int UPDATE_PARAM_ALL = -1;
 
-    // When setCameraParametersWhenIdle() is called, we accumulate the subsets
+    //  When setCameraParametersWhenIdle() is called, we accumulate the subsets
     // needed to be updated in mUpdateSet.
     private int mUpdateSet;
 
@@ -1327,7 +1327,6 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         mPreferences = new ComboPreferences(this);
         CameraSettings.upgradeGlobalPreferences(mPreferences.getGlobal());
         mCameraId = getPreferredCameraId(mPreferences);
-
         mContentResolver = getContentResolver();
         powerShutter(mPreferences);
         // To reduce startup time, open the camera and start the preview in
@@ -1819,6 +1818,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         mFaceView.setMirror(info.facing == CameraInfo.CAMERA_FACING_FRONT);
         mFaceView.resume();
         mFocusManager.setFaceView(mFaceView);
+        mPreviewFrameLayout.setCameraOrientation(info.orientation);
     }
 
     @Override
@@ -1961,6 +1961,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
             mCameraDevice = null;
             setCameraState(PREVIEW_STOPPED);
             mFocusManager.onCameraReleased();
+            mSurfaceTexture = null;
         }
     }
 
@@ -2080,7 +2081,6 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
             // Use the same area for focus and metering.
             mParameters.setMeteringAreas(mFocusManager.getMeteringAreas());
         }
-
         // Set picture size.
         String pictureSize = mPreferences.getString(
                 CameraSettings.KEY_PICTURE_SIZE, null);
@@ -2111,14 +2111,11 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         Size original = mParameters.getPreviewSize();
         if (!original.equals(optimalSize)) {
             mParameters.setPreviewSize(optimalSize.width, optimalSize.height);
-
             // Zoom related settings will be changed for different preview
             // sizes, so set and read the parameters to get latest values
             mCameraDevice.setParameters(mParameters);
             mParameters = mCameraDevice.getParameters();
         }
-        Log.v(TAG, "Preview size is " + optimalSize.width + "x" + optimalSize.height);
-
         // Since change scene mode may change supported values,
         // Set scene mode first,
 
@@ -2431,6 +2428,8 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         boolean mirror = (info.facing == CameraInfo.CAMERA_FACING_FRONT);
         mFocusManager.setMirror(mirror);
         mFocusManager.setParameters(mInitialParams);
+        mPreviewFrameLayout.setCameraOrientation(info.orientation);
+        setPreviewFrameLayoutAspectRatio();
         startPreview();
         setCameraState(IDLE);
         startFaceDetection();
@@ -2549,5 +2548,8 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         // Set the preview frame aspect ratio according to the picture size.
         Size size = mParameters.getPictureSize();
         mPreviewFrameLayout.setAspectRatio((double) size.width / size.height);
+        CameraInfo info = CameraHolder.instance().getCameraInfo()[mCameraId];
+        mPreviewFrameLayout.setCameraOrientation(info.orientation);
     }
 }
+

src/com/android/camera/PreviewFrameLayout.java
@@ -21,6 +21,8 @@ import android.content.res.Configuration;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.hardware.Camera.CameraInfo;
+import android.util.Log;
 
 /**
  * A layout which handles the preview aspect ratio.
@@ -34,10 +36,17 @@ public class PreviewFrameLayout extends RelativeLayout {
     private double mAspectRatio;
     private View mBorder;
     private OnSizeChangedListener mListener;
+    private int mCameraOrientation;
+    private static final String TAG = "PreviewFrameLayout";
 
     public PreviewFrameLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         setAspectRatio(4.0 / 3.0);
+        mCameraOrientation = 90;
+    }
+
+    public void setCameraOrientation(int orientation){
+        mCameraOrientation = orientation;
     }
 
     @Override
@@ -79,16 +88,23 @@ public class PreviewFrameLayout extends RelativeLayout {
         // Resize the preview frame with correct aspect ratio.
         previewWidth -= hPadding;
         previewHeight -= vPadding;
-        if (previewWidth > previewHeight * mAspectRatio) {
-            previewWidth = (int) (previewHeight * mAspectRatio + .5);
+        if(mCameraOrientation % 180 == 0) {
+            if (previewWidth > previewHeight * mAspectRatio) {
+                previewHeight = (int) (previewWidth / mAspectRatio + .5);
+            } else {
+                previewWidth = (int) (previewHeight * mAspectRatio + .5);
+            }
         } else {
-            previewHeight = (int) (previewWidth / mAspectRatio + .5);
+            if (previewWidth > previewHeight * mAspectRatio) {
+                previewWidth = (int) (previewHeight * mAspectRatio + .5);
+            } else {
+                previewHeight = (int) (previewWidth / mAspectRatio + .5);
+            }
         }
 
         // Add the padding of the border.
         previewWidth += hPadding;
         previewHeight += vPadding;
-
         // Ask children to follow the new preview dimension.
         super.onMeasure(MeasureSpec.makeMeasureSpec(previewWidth, MeasureSpec.EXACTLY),
                 MeasureSpec.makeMeasureSpec(previewHeight, MeasureSpec.EXACTLY));

src/com/android/camera/Util.java
@@ -416,12 +416,11 @@ public class Util {
     public static Size getOptimalPreviewSize(Activity currentActivity,
             List<Size> sizes, double targetRatio) {
         // Use a very small tolerance because we want an exact match.
-        final double ASPECT_TOLERANCE = 0.001;
+        final double ASPECT_TOLERANCE = 0.00001;
         if (sizes == null) return null;
-
         Size optimalSize = null;
         double minDiff = Double.MAX_VALUE;
-
+        double minAspectRatioDiff = Double.MAX_VALUE;
         // Because of bugs of overlay and layout, we sometimes will try to
         // layout the viewfinder in the portrait orientation and thus get the
         // wrong size of preview surface. When we change the preview size, the
@@ -435,17 +434,35 @@ public class Util {
         // Try to find an size match aspect ratio and size
         for (Size size : sizes) {
             double ratio = (double) size.width / size.height;
+            if(size.height <= targetHeight && Math.abs(ratio - targetRatio) < minAspectRatioDiff) {
+                minAspectRatioDiff = Math.abs(ratio - targetRatio);
+            }
+        }
+
+       /* for (Size size : sizes) {
+            double ratio = (double) size.width / size.height;
             if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
             if (Math.abs(size.height - targetHeight) < minDiff) {
                 optimalSize = size;
                 minDiff = Math.abs(size.height - targetHeight);
+           }
+       } */
+
+        for(Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            double aspectRatioDiff = Math.abs(ratio - targetRatio);
+            if(size.height <= targetHeight) {
+                if(aspectRatioDiff - minAspectRatioDiff > ASPECT_TOLERANCE)
+                    continue;
+                if(optimalSize == null || optimalSize.height < size.height) {
+                    optimalSize = size;
+                }
             }
         }
 
         // Cannot find the one match the aspect ratio. This should not happen.
         // Ignore the requirement.
         if (optimalSize == null) {
-            Log.w(TAG, "No preview size match the aspect ratio");
             minDiff = Double.MAX_VALUE;
             for (Size size : sizes) {
                 if (Math.abs(size.height - targetHeight) < minDiff) {

commit bdeb976511d1b42fb7a6ca20eeaa18a1b4b9b553

Camera : Fix camera crash
Commit 61b3662ccce0beacacd5e404dd1b21300a263605 introduced :
mSurfaceTexture = null; inside closeCamera().

This is incorrect since, inside stopCamera, right after calling closeCamera
it does a null check in order to release the textures, pre-setting it to null
fails the condition and they never get released.

closeCamera();
if (mSurfaceTexture != null) {
   mCameraScreenNail.releaseSurfaceTexture();
   mSurfaceTexture = null;
}

To reproduce crash :

- Open camera
- Press home button
- Relaunch camera via lockscreen or homescreen icon
- Repeat if necessary

Change-Id: I47c3009ce3820307bf3d742c721720e63c51484e

src/com/android/camera/Camera.java
@@ -1961,7 +1961,6 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
             mCameraDevice = null;
             setCameraState(PREVIEW_STOPPED);
             mFocusManager.onCameraReleased();
-            mSurfaceTexture = null;
         }
     }
 