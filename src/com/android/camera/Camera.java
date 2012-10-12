12d0c20847aa709345f1c1b07de17c655e9874b3
Danesh Mondegarian <daneshm90@gmail.com>
2012-10-12 18:47:58 -0400

    Camera : Ignore keyevents when in gallery
    When in gallery view, ignore any keyevents.
    
    Change-Id: I6d412327d603f204435302c1955b4dcd3bc942fc
    http://goo.gl/9FEGw

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -1989,6 +1989,9 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (!mShowCameraAppView) {
+            return false;
+        }
         switch (keyCode) {
             case KeyEvent.KEYCODE_FOCUS:
                 if (mFirstTimeInitialized && event.getRepeatCount() == 0) {
@@ -2050,6 +2053,9 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (!mShowCameraAppView) {
+            return false;
+        }
         switch (keyCode) {
             case KeyEvent.KEYCODE_FOCUS:
                 if (mFirstTimeInitialized) {
