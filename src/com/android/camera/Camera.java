17be2a75efc07712923cf2865f12d3f5e3bce65e
Danesh Mondegarian <daneshm90@gmail.com>
2012-10-16 00:00:55 -0400

    Camera : Fix Ignore keyevents
    Call super's onKeyUp/onKeyDown instead.
    
    Change-Id: I054a58f1d0758edafdd4505d41401f3f8f53dda0

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -1990,7 +1990,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (!mShowCameraAppView) {
-            return false;
+            return super.onKeyDown(keyCode, event);
         }
         switch (keyCode) {
             case KeyEvent.KEYCODE_FOCUS:
@@ -2054,7 +2054,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
         if (!mShowCameraAppView) {
-            return false;
+            return super.onKeyUp(keyCode, event);
         }
         switch (keyCode) {
             case KeyEvent.KEYCODE_FOCUS:
