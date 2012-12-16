d02fa2b689d9c47de958cf5549a69d7149bb8cf4
jt1134 <jt1134@gmail.com>
2012-12-16 00:25:17 -0600

    Camera: add color effect, ISO, and JPEG quality settings
    Change-Id: I0d6a3cf864e618e3698baa42f8a11c0fa9a908a7

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2264,12 +2264,27 @@ public class PhotoModule
         }
 
         // Set JPEG quality.
-        int jpegQuality = CameraProfile.getJpegEncodingQualityParameter(mCameraId,
-                CameraProfile.QUALITY_HIGH);
+        int jpegQuality = Integer.parseInt(mPreferences.getString(
+                CameraSettings.KEY_JPEG,
+                mActivity.getString(R.string.pref_camera_jpeg_default)));
         mParameters.setJpegQuality(jpegQuality);
 
         // For the following settings, we need to check if the settings are
         // still supported by latest driver, if not, ignore the settings.
+
+        // Set ISO speed.
+        String isoMode = mPreferences.getString(CameraSettings.KEY_ISO_MODE,
+                mActivity.getString(R.string.pref_camera_iso_default));
+        if (Util.isSupported(isoMode, mParameters.getSupportedIsoValues()))
+                mParameters.setISOValue(isoMode);
+
+        // Color effect
+        String colorEffect = mPreferences.getString(
+                CameraSettings.KEY_COLOR_EFFECT,
+                mActivity.getString(R.string.pref_camera_coloreffect_default));
+        if (Util.isSupported(colorEffect, mParameters.getSupportedColorEffects())) {
+            mParameters.setColorEffect(colorEffect);
+        }
 
         // Set exposure compensation
         int value = CameraSettings.readExposure(mPreferences);
--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -2014,6 +2014,14 @@ public class VideoModule implements CameraModule,
                 CameraProfile.QUALITY_HIGH);
         mParameters.setJpegQuality(jpegQuality);
 
+        // Color effect
+        String colorEffect = mPreferences.getString(
+                CameraSettings.KEY_COLOR_EFFECT,
+                mActivity.getString(R.string.pref_camera_coloreffect_default));
+        if (Util.isSupported(colorEffect, mParameters.getSupportedColorEffects())) {
+            mParameters.setColorEffect(colorEffect);
+        }
+
         mActivity.mCameraDevice.setParameters(mParameters);
         // Keep preview size up to date.
         mParameters = mActivity.mCameraDevice.getParameters();
