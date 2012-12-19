b9b133451de711c58c6097f965871e0a63f66a28
Michael Kolb <kolby@google.com>
2012-12-19 14:45:32 -0800

    Fix NPE in config change
      Bug: 7894406
    
    Config change occured before camera open finished
    
    Change-Id: I5ea6f18b53c28006e8f00edf98f2d14f98f89b2f

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -855,6 +855,7 @@ public class PhotoModule
     }
 
     private void updateOnScreenIndicators() {
+        if (mParameters == null) return;
         updateSceneOnScreenIndicator(mParameters.getSceneMode());
         updateExposureOnScreenIndicator(CameraSettings.readExposure(mPreferences));
         updateFlashOnScreenIndicator(mParameters.getFlashMode());
