1fc1065d6f8d8e6ff6e9df637e260c8491d86a5f
maxwen <max.weninger@gmail.com>
2012-12-24 12:18:08 +0100

    fix for correct handling of video snapshot feature
    the mPaused condition is checked wrong
    
    Change-Id: Ibe66dcec5bc6248159a0b914e5f24f2986d8b8fe

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -2539,7 +2539,7 @@ public class VideoModule implements CameraModule,
             return;
         }
 
-        if (!mPaused || mSnapshotInProgress || effectsActive()
+        if (mPaused || mSnapshotInProgress || effectsActive()
                 || !Util.isVideoSnapshotSupported(mParameters)) {
             return;
         }
