64e2e9752f0f86a91700f7677936d1a61b92ca38
jt1134 <jt1134@gmail.com>
2013-02-24 00:22:33 -0600

    Camera: add exposure settings to video camera
    Change-Id: I350484e93f88d229362dee663277a06f132fd4a3

--- src/com/android/camera/VideoController.java
+++ src/com/android/camera/VideoController.java
@@ -57,6 +57,7 @@ public class VideoController extends PieController
         float sweep = FLOAT_PI_DIVIDED_BY_TWO / 2;
 
         addItem(CameraSettings.KEY_VIDEOCAMERA_FLASH_MODE, FLOAT_PI_DIVIDED_BY_TWO - sweep, sweep);
+        addItem(CameraSettings.KEY_EXPOSURE, 3 * FLOAT_PI_DIVIDED_BY_TWO - sweep, sweep);
         addItem(CameraSettings.KEY_WHITE_BALANCE, 3 * FLOAT_PI_DIVIDED_BY_TWO + sweep, sweep);
         if (group.findPreference(CameraSettings.KEY_CAMERA_ID) != null) {
             PieItem item = makeItem(R.drawable.ic_switch_video_facing_holo_light);
