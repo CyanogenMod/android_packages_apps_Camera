c6b475e70d7cad6e7b0622884f4d6e06d126ae78
David Hays <Dhays90@gmail.com>
2013-03-04 11:17:57 -0600

    Fixing Partition Size

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1946156032
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 12482248704
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 27917287424
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/htc/m7/kernel
@@ -26,3 +26,28 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 TARGET_RECOVERY_INITRC := device/htc/m7/recovery/init.rc
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
+
+
+# cat /proc/emmc:
+# dev: size erasesize name
+# mmcblk0p19: 000ffa00 00000200 "misc"
+# mmcblk0p34: 00fffe00 00000200 "recovery"
+# mmcblk0p33: 01000000 00000200 "boot"
+# mmcblk0p35: 73fffc00 00000200 "system"
+# mmcblk0p26: 00140200 00000200 "local"
+# mmcblk0p36: 27fffe00 00000200 "cache"
+# mmcblk0p37: 680000000 00000200 "userdata"
+# mmcblk0p22: 01400000 00000200 "devlog"
+# mmcblk0p24: 00040000 00000200 "pdata"
+# mmcblk0p27: 00010000 00000200 "extra"
+# mmcblk0p31: 04b00200 00000200 "radio"
+# mmcblk0p16: 03c00400 00000200 "adsp"
+# mmcblk0p15: 00100000 00000200 "dsps"
+# mmcblk0p17: 007ffa00 00000200 "radio_config"
+# mmcblk0p20: 00400000 00000200 "modem_st1"
+# mmcblk0p21: 00400000 00000200 "modem_st2"
+# mmcblk0p28: 00100000 00000200 "cdma_record"
+# mmcblk0p18: 02000000 00000200 "reserve_1"
+# mmcblk0p30: 034ffa00 00000200 "reserve_2"
+# mmcblk0p32: 05fffc00 00000200 "reserve_3"
+# mmcblk0p29: 06069e00 00000200 "reserve"
