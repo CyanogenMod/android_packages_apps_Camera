6325a6a647bb9bf000b0ea4e74fba37de102fe44
David Hays <Dhays90@gmail.com>
2013-04-10 11:28:14 -0500

    Tweaks

--- device_m7.mk
+++ device_m7.mk
@@ -1,17 +1,33 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-$(call inherit-product-if-exists, vendor/htc/m7/m7-vendor.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk
 
 DEVICE_PACKAGE_OVERLAYS += device/htc/m7/overlay
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
     device/htc/m7/ramdisk/fstab.m7:root/fstab.m7 \
-	device/htc/m7/ramdisk/init:root/init \
+    device/htc/m7/ramdisk/init:root/init \
     device/htc/m7/ramdisk/init.m7.rc:root/init.m7.rc \
-	device/htc/m7/ramdisk/init.m7.usb.rc:root/init.m7.usb.rc \
-	device/htc/m7/ramdisk/init.rc:root/init.rc \
-	device/htc/m7/ramdisk/ueventd.m7.rc:root/ueventd.m7.rc \
+    device/htc/m7/ramdisk/init.m7.usb.rc:root/init.m7.usb.rc \
+    device/htc/m7/ramdisk/init.rc:root/init.rc \
+    device/htc/m7/ramdisk/ueventd.m7.rc:root/ueventd.m7.rc \
 
 # QCOM Boot ramdisk setup
 PRODUCT_PACKAGES += \
@@ -70,12 +86,12 @@ PRODUCT_COPY_FILES += \
 # Keylayouts and Keychars
 PRODUCT_COPY_FILES += \
     device/htc/m7/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-	device/htc/m7/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
-	device/htc/m7/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
-	device/htc/m7/keylayout/keypad_8960.kl:system/usr/keylayout/keypad_8960.kl \
-	device/htc/m7/keylayout/projector-Keypad.kl:system/usr/keylayout/projector-Keypad.kl \
-	device/htc/m7/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-	device/htc/m7/keylayout/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl \
+    device/htc/m7/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
+    device/htc/m7/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/m7/keylayout/keypad_8960.kl:system/usr/keylayout/keypad_8960.kl \
+    device/htc/m7/keylayout/projector-Keypad.kl:system/usr/keylayout/projector-Keypad.kl \
+    device/htc/m7/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/htc/m7/keylayout/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl \
 	
 # Input device config
 PRODUCT_COPY_FILES += \
@@ -173,7 +189,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
@@ -194,7 +211,7 @@ PRODUCT_COPY_FILES += \
     device/htc/m7/configs/gps.conf:system/etc/gps.conf
 	
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=mtp
+    persist.sys.usb.config=mtp
 	
 # Common build properties
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -220,10 +237,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=HTC8960RIL \
     ro.use_data_netmgrd=true \
     wifi.interface=wlan0
-	
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Set build date
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi xxhdpi
 PRODUCT_NAME := full_m7
 PRODUCT_DEVICE := m7
+
+# call the proprietary setup
+$(call inherit-product-if-exists, vendor/htc/m7/m7-vendor.mk)
+
+# call dalvik heap config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
