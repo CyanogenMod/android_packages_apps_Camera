91b6f4e00ccb59ade893f249b537f0e8b92ac343
Matt Filetto <matt.filetto@gmail.com>
2013-04-16 18:05:58 -0700

    Camera: fix pinch to zoom for some devices
    Fixes Pinch to Zoom calibration on pyramid/shooter and maybe others
    
    Original author tartfu (hope your feeling well man!)
    
    Change-Id: I421f29e120de640ed47543df689a680aa2d6b7bf

--- src/com/android/camera/ui/ZoomRenderer.java
+++ src/com/android/camera/ui/ZoomRenderer.java
@@ -48,6 +48,8 @@ public class ZoomRenderer extends OverlayRenderer
     private int mZoomSig;
     private int mZoomFraction;
     private Rect mTextBounds;
+    private float mBeginSpan;
+    private float mBeginCircle;
 
     public interface OnZoomChangedListener {
         void onZoomStart();
@@ -123,12 +125,14 @@ public class ZoomRenderer extends OverlayRenderer
                 mTextPaint);
     }
 
+    
     @Override
     public boolean onScale(ScaleGestureDetector detector) {
-        final float sf = detector.getScaleFactor();
-        float circle = (int) (mCircleSize * sf * sf);
+        float span = (detector.getCurrentSpan()-mBeginSpan)/2.5f;
+        float circle = (int) (mBeginCircle + span);
         circle = Math.max(mMinCircle, circle);
         circle = Math.min(mMaxCircle, circle);
+
         if (mListener != null && (int) circle != mCircleSize) {
             mCircleSize = (int) circle;
             int zoom = mMinZoom + (int) ((mCircleSize - mMinCircle) * (mMaxZoom - mMinZoom) / (mMaxCircle - mMinCircle));
@@ -140,6 +144,8 @@ public class ZoomRenderer extends OverlayRenderer
     @Override
     public boolean onScaleBegin(ScaleGestureDetector detector) {
         setVisible(true);
+        mBeginSpan=detector.getCurrentSpan();
+        mBeginCircle=mCircleSize;
         if (mListener != null) {
             mListener.onZoomStart();
         }
