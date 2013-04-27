03ec96f9c1841332b94b9cb6931579340be80fdb
Jared Caliendo <kushdeck@gmail.com>
2013-04-27 00:19:18 -0700

    playing with derpy ramdisk still.
    Change-Id: I2f049f8adf9599f94c6975d5c09d575d1684c424

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -24,7 +24,7 @@
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
 
-# inherit from common apq8064
+# inherit from common msm8960
 -include device/htc/msm8960-common/BoardConfigCommon.mk
 
 # Flags
