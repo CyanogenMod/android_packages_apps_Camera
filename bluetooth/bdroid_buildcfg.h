492e49930febb15568eb16c28ebb42315d8b23d4
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-04-28 09:05:36 +0200

    m7-common: bt firmware settlement delay

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "One"
+#define BTM_DEF_LOCAL_NAME   "HTC One"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #endif
