ce0b368c1372c9a0b5604d268882297c27f4fcee
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-10 18:43:09 +0200

    m7: fix property perms append
    Change-Id: I235e94d158547c9c7f15efb2436c2c307e1fd0da

--- include/device_perms.h
+++ include/device_perms.h
@@ -18,7 +18,7 @@
 #define DEVICE_PERMS_H
 
 #define PROPERTY_PERMS_APPEND \
-    { "persist.camera.",  AID_MEDIA,    0 },
+    { "persist.camera.",  AID_MEDIA,    0 }, \
     { "camera.",          AID_MEDIA,    0 },
 
 #endif /* DEVICE_PERMS_H */
