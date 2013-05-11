e68b5c132fa5e714f062fc8d3b752cf1ca3b2b08
Ethan Chen <intervigil@gmail.com>
2013-05-11 15:24:10 -0700

    m7-common: audio: use separated use cases for input, voice speaker
    Also use Fluence dual mic only in calls.
    
    Change-Id: I1020c47cb8326f690f0b4270267a465c7a830057

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,6 +43,9 @@ COMMON_GLOBAL_CFLAGS += -DHTCLOG
 
 # Audio
 BOARD_AUDIO_CAF_LEGACY_INPUT_BUFFERSIZE := true
+BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
+BOARD_USES_SEPERATED_AUDIO_INPUT := true  # use distinct voice recog/camcorder use cases
+BOARD_USES_SEPERATED_VOICE_SPEAKER := true  # use distinct voice speaker use case
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH_BCM := true
