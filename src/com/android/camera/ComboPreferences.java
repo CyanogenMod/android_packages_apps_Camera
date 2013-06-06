15e80c5e4d69f190afee0422cae1c7432f914d89
Danny Baumann <dannybaumann@web.de>
2013-06-06 11:05:48 +0200

    Don't mess around with pref local ID for power shutter initialization.
    It confuses the pref initialization, causing the back facing camera resolution
    being overwritten by the front facing camera resolution.
    Instead, make power shutter a global (camera independent) setting.
    
    JIRA:CYAN-752
    Change-Id: I5b1c89bcb42ed64feb06a8562ee5e468183f30f0

--- src/com/android/camera/ComboPreferences.java
+++ src/com/android/camera/ComboPreferences.java
@@ -80,6 +80,7 @@ public class ComboPreferences implements SharedPreferences, OnSharedPreferenceCh
                 || key.equals(CameraSettings.KEY_CAMERA_FIRST_USE_HINT_SHOWN)
                 || key.equals(CameraSettings.KEY_VIDEO_FIRST_USE_HINT_SHOWN)
                 || key.equals(CameraSettings.KEY_VIDEO_EFFECT)
+                || key.equals(CameraSettings.KEY_POWER_SHUTTER)
                 || key.equals(CameraSettings.KEY_STORAGE);
     }
 
