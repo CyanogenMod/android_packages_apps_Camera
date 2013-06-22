7f081fc719756d91c080ec3785a7167695bbd70d
Chirayu Desai <cdesai@cyanogenmod.org>
2013-06-22 21:37:20 +0530

    Add a setting for Shutter Speed
    Change-Id: Ieebe50a4be04e7fea9301da63c008e36f4ec0dbd

--- src/com/android/camera/Util.java
+++ src/com/android/camera/Util.java
@@ -122,6 +122,10 @@ public class Util {
         return TRUE.equals(params.get(VIDEO_HDR_SUPPORTED));
     }
 
+    public static boolean isShutterSpeedSupported(Parameters params) {
+         return sShutterSpeed;
+    }
+
     @TargetApi(ApiHelper.VERSION_CODES.ICE_CREAM_SANDWICH)
     public static boolean isMeteringAreaSupported(Parameters params) {
         if (ApiHelper.HAS_CAMERA_METERING_AREA) {
@@ -176,6 +180,8 @@ public class Util {
     // Send magic command to hardware for Samsung ZSL
     private static boolean sSendMagicSamsungZSLCommand;
 
+    private static boolean sShutterSpeed;
+
     private static SpeechRecognizer mSpeechRecognizer;
     private static Intent mSpeechRecognizerIntent;
     private static String[] mShutterWords;
@@ -216,6 +222,9 @@ public class Util {
 
         sSendMagicSamsungZSLCommand = context.getResources().getBoolean(
                 R.bool.sendMagicSamsungZSLCommand);
+
+        sShutterSpeed = context.getResources().getBoolean(
+                R.bool.enableShutterSpeed);
 
         /* Voice Shutter */
         mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(context);
